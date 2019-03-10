87c41d355270bd72ccf5139808720a775efe1d81
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2019-03-10 12:23:09 -0300

    Criaçao do formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,7 +33,8 @@ import { EditarReceitaComponent } from './receitas/editar-receita/editar-receita
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ListaComprasService],
   bootstrap: [AppComponent]
--- src/app/receitas/editar-receita/editar-receita.component.ts
+++ src/app/receitas/editar-receita/editar-receita.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { ReceitasService } from 'src/app/services/receitas.service';
 
 @Component({
   selector: 'app-editar-receita',
@@ -10,16 +12,71 @@ export class EditarReceitaComponent implements OnInit {
 
   id: number;
   modoEdicao = false;
+  receitaForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private receitaService: ReceitasService) { }
 
   ngOnInit() {
     this.route.params
       .subscribe((params: Params) => {
         this.id = +params['id'];
         this.modoEdicao = params['id'] != null;
+        this.initForm();
       }
     );
+  }
+
+  getControls() {
+    const controls = (<FormArray> this.receitaForm.get('ingredientes')).controls ;
+    console.log(controls);
+    return controls;
+  }
+
+  onSubmit() {
+    console.log(this.receitaForm);
+  }
+
+  onAddIngrediente() {
+    (<FormArray>this.receitaForm.get('ingredientes')).push(
+      new FormGroup({
+        nome: new FormControl(null, Validators.required),
+        quantidade: new FormControl(null, [Validators.required,
+        Validators.pattern(/^[1-9]+[0-9]*$/)])
+      })
+    );
+  }
+
+  private initForm() {
+    let nomeReceita = '';
+    let caminhoImagemReceita = '';
+    let descricaoRecceita = '';
+    const receitaIngredientes = new FormArray([]);
+
+    if (this.modoEdicao) {
+      const receita = this.receitaService.getReceita(this.id);
+      nomeReceita = receita.nome;
+      caminhoImagemReceita = receita.caminhoImagem;
+      descricaoRecceita = receita.descricao;
+
+      if (receita['ingredientes']) {
+        for (const ingrediente of receita.ingredientes) {
+          receitaIngredientes.push(
+            new FormGroup({
+              nome: new FormControl(ingrediente.nome, Validators.required),
+              quantidade: new FormControl(ingrediente.quantidade, [Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/)])
+            })
+          );
+        }
+      }
+    }
+    this.receitaForm = new FormGroup({
+      nome: new FormControl(nomeReceita, Validators.required),
+      imagePath: new FormControl(caminhoImagemReceita, Validators.required),
+      descricao: new FormControl(descricaoRecceita, Validators.required),
+      ingredientes: receitaIngredientes
+    });
   }
 
 }
